f15e258ff1a0ee82cded1685bce94a4b73d70f07
Vedant Kothari <vkothari50@stuy.edu>
2024-09-16 11:46:31 -0400

    Added k4

---
+++ 04_rando-dict/krewes.py
@@ -0,0 +1,27 @@
+# Vedant Kothari
+# K^3
+# SoftDev
+# Learn more about dictionaries and random selection
+# <2024>-<09>-<13>
+# time spent: 0.3 hours
+import random
+krewes = {
+           4: [ 
+		'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+		'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+		'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+		'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+		],
+           5: [ 
+                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
+              ]
+         }
+
+r1 = random.randint(4, 5)
+l1 = krewes.get(r1);
+r2 = random.randint(0, len(l1) - 1);
+print(l1[r2]);
+
