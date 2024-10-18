25ec0d34752c05c169c67bee05dd05425e7c7f1b
Vedant Kothari <vkothari50@stuy.edu>
2024-10-17 23:01:52 -0400

    Added more in-line comments

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -17,21 +17,21 @@ roster = ["Kyle Lee, Vedant Kothari, Suhana Kumar"]
 
 @app.route('/')
 def primary():
-    if 'username' in session:
+    if 'username' in session: #Checks if there is an active session currently
         return redirect(url_for('submit'))
-    return render_template('submit.html',T_name=T_name,roster=roster)
+    return render_template('submit.html',T_name=T_name,roster=roster) #Renders submit.html on the page if there is no active session
 
 @app.route('/submit', methods=['POST'])
 def submit():
     username = request.form.get('username')
-    session['username'] = username
+    session['username'] = username #Stores username in a session
     return redirect(url_for('welcome'))
 
 @app.route('/welcome')
 def welcome():
     if 'username' in session:
         username = session['username']
-        greeting = "Hey there "+username+". We hope you like this page"
+        greeting = "Hey there "+username+". We hope you like this page" #Returns greeting once user submits username or is already logged in
         method_used = request.method
         explanation = '''
         This is a flask app which checks if you login via a correct username. You will be logged in until you click logout. The app stores sessions via cookies, and the session cookie is sent to your browser.
@@ -42,7 +42,7 @@ def welcome():
 
 @app.route('/unsubmit')
 def unsubmit():
-    username = session.pop('username', None)
+    username = session.pop('username', None) #Removes user from session
     return render_template('unsubmit.html', T_name=T_name, roster=roster)
 
 if __name__ == '__main__':
