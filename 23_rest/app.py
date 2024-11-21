ea002b38b486a4bbfc54dca89d4efcd6cd98cca5
Vedant Kothari <vkothari50@stuy.edu>
2024-11-21 06:35:32 -0500

    Finished k23

---
+++ 23_rest/app.py
@@ -0,0 +1,39 @@
+'''
+Vedant Kothari, Will Nzeuton
+Team Name: Nzeutons
+K23 - A RESTful Journey Skyward
+2024-11-20
+Time Spent: 1
+'''
+
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+
+from urllib import request
+import json
+
+
+key = "dpf7J5K33dTVC0xYPZzQVrgy5z3eiwqnTKraBvd4"
+
+app = Flask(__name__)
+
+
+
+
+@app.route("/")
+def index():
+    url = f"https://api.nasa.gov/planetary/apod?api_key={key}&count=1"
+
+    with request.urlopen(url) as response:
+        response = response.read().decode('utf-8')
+
+        jsonify = json.loads(response)[0]
+        
+    print(jsonify)
+    return render_template( 'index.html', date = jsonify['date'], description = jsonify['explanation'], title = jsonify['title'], url = jsonify['url'])
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
